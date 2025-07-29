e7a380164ece863a7266bc98af3d74495fc569d8
baizta404 <chowdhuryhridoy261@gmail.com>
2025-07-29 22:50:06 +0600

    fixed error

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from tasks.views import show_task,show_sepecific_task
 
 urlpatterns=[
-    path('show-task/',show_task)
+    path('show-task/',show_task),
     path('show-task/<int:id>',show_sepecific_task)
 ]
