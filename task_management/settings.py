808cb4553825bb488b6db9067f3c4612190ff253
baizta404 <chowdhuryhridoy261@gmail.com>
2025-08-06 10:33:26 +0600

    module-5 uploaded

--- task_management/settings.py
+++ task_management/settings.py
@@ -74,13 +74,25 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#For sqllite3 database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#For postgreSQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_management',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
+#Many to Many Relationship
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+
+#Many to One Relationship
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    start_date = models.DateField()
+
+
 # Create your models here.
+class Task(models.Model):
+    project = models.ForeignKey(Project,
+                                on_delete=models.CASCADE,
+                                default = 1)
+    assigned_to = models.ManyToManyField(Employee,related_name='tasks')
+    title = models.CharField(max_length = 250)
+    description = models.TextField()
+    due_date = models.DateField()
+    is_complited = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+#one to one
+class TaskDetails(models.Model):
+    HIGH = 'H'
+    MEDIUM = 'M'
+    LOW = 'L'
+    PRIORITY_OPTIONS = (
+        (HIGH,'High'),
+        (MEDIUM,'Medium'),
+        (LOW,'Low')
+    )
+    task = models.OneToOneField(Task,
+                                on_delete = models.CASCADE,
+                                related_name = "details")
+    assigned_to = models.CharField(max_length = 100)
+    priority = models.CharField(max_length = 1 ,choices = PRIORITY_OPTIONS,default = LOW)
+
+#ekta task ->onek gula employee
+#ekjon employee -> onek gula task
+#ekta project a onek task
+# >>> from tasks.models import Employee,Task
+# >>> employee = Employee.objects.create(name="Romjan Ali",email="ramjan@gmail.com")
+# >>> employee2 = Employee.objects.create(name="Minjaj Adil",email="minhaj@gmail.com")
+# >>> task = Task.objects.create(title="Notun taska ri",description="Ki ar hoibo re bhai emni",due_date ="2025-12-25")
+# >>> task.assigned_to.add(employee)ta
