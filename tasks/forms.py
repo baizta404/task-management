af01fbc6b0bda04098de0ff8fb630ce95c3aa611
baizta404 <chowdhuryhridoy261@gmail.com>
2025-09-19 17:24:49 +0600

    Module-6 done

--- tasks/forms.py
+++ tasks/forms.py
@@ -16,24 +16,63 @@ class TaskForm(forms.Form):
         ]
 
 
-class TaskModelForm(forms.ModelForm):
+class StyledFormMixin:
+    """Maxin to apply style to form fields"""
+    default_classes= "border-2 border-gray-300 w-full rounded-lg shadow-sm"
+    
+
+    def apply_styled_widgets(self):
+        for field_name,field in self.fields.items():
+            if isinstance(field.widget,forms.TextInput):
+                field.widget.attrs.update({
+                    'class': self.default_classes,
+                    'placeholder': f'Enter {field.label.lower()}'
+                })
+            elif isinstance(field.widget,forms.Textarea):
+                field.widget.attrs.update({
+                    'class': f'{self.default_classes} resize-none',
+                    'placeholder': f'Enter {field.label.lower()}',
+                    'rows':5
+                })
+            elif isinstance(field.widget,forms.SelectDateWidget):
+                field.widget.attrs.update({
+                    'class': "border-2 border-gray-300 rounded-lg shadow-sm"
+                })
+            elif isinstance(field.widget,forms.CheckboxSelectMultiple):
+                field.widget.attrs.update({
+                    'class':'space-y-2'
+                })
+
+
+#mixin use korte hoile kintu Mixin ta inheritance korte hobe .always first a bosaite hobe
+class TaskModelForm(StyledFormMixin,forms.ModelForm):
     class Meta:
         model = Task
         #fields = '__all__' for selecting all fields
         fields =['title','description','due_date','assigned_to']
         #exclude=["",""] just mention which should exclude from all
-        widgets ={
-            'title':forms.TextInput(attrs={
-                'class':"border-2 border-gray-300 w-full rounded-lg shadow-sm",
-                'placeholder': "Enter Tasks's Title.."
-            }),
-            'due_date': forms.SelectDateWidget({
-                'class': "border-2 border-gray-300 rounded-lg shadow-sm"
-            }),
-            'assigned_to':forms.CheckboxSelectMultiple(),
-            'description':forms.Textarea(attrs={
-                "class":"border-2 border-gray-300 w-full rounded-lg shadow-sm resize-none",
-                'row':5,
-                'placeholder':"Write detail's about the task"
-            })
+        widgets={
+            'due_date':forms.SelectDateWidget,
+            'assigned_to':forms.CheckboxSelectMultiple
         }
+        """Manual adding without mixin : allways widgets need to be in Meta Class"""
+        # widgets ={
+        #     'title':forms.TextInput(attrs={
+        #         'class':"border-2 border-gray-300 w-full rounded-lg shadow-sm",
+        #         'placeholder': "Enter Tasks's Title.."
+        #     }),
+        #     'due_date': forms.SelectDateWidget({
+        #         'class': "border-2 border-gray-300 rounded-lg shadow-sm"
+        #     }),
+        #     'assigned_to':forms.CheckboxSelectMultiple(),
+        #     'description':forms.Textarea(attrs={
+        #         "class":"border-2 border-gray-300 w-full rounded-lg shadow-sm resize-none",
+        #         'row':5,
+        #         'placeholder':"Write detail's about the task"
+        #     })
+        # }
+
+    """def __init__ for self appllying mixin| Meta Class er baire thakte hobe"""
+    def __init__(self,*args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.apply_styled_widgets()
