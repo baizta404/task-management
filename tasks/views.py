97d78f007993062d54bcd6d6394b62704dbe191c
baizta404 <chowdhuryhridoy261@gmail.com>
2026-07-25 22:24:16 +0600

    Dynamic Urls added

--- tasks/views.py
+++ tasks/views.py
@@ -6,3 +6,7 @@ def home(request):
     return HttpResponse('Welcome to home')
 def show_task(request):
     return HttpResponse('Oi cha testingg show task')
+def show_specific_task(request,id):
+    print(id)
+    print('type of id', type(id))
+    return HttpResponse(f'{id} showing specing task')
