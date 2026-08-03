d7508e3d99330c88a9a3af69bacf04b36ec7ffa1
baizta404 <chowdhuryhridoy261@gmail.com>
2026-08-03 19:35:12 +0600

    5.6 one to many relationship

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Task(models.Model):
+    project = models.ForeignKey("Project",on_delete=models.CASCADE,default=1)
     title = models.CharField(max_length=250)
     description = models.TextField()
     due_date = models.DateField()
@@ -22,3 +23,7 @@ class TaskDetail(models.Model):
     assigned_to = models.CharField(max_length=100)
     priority = models.CharField(
         max_length=1,choices=PRIORITY_OPTIONS,default=LOW) 
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    start_date = models.DateField()
