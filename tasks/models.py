f3ebcacf57c3b6b419566ac28edba89aa65596e6
baizta404 <chowdhuryhridoy261@gmail.com>
2026-08-03 21:18:06 +0600

    many to many & reverse relationship & related names

--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email= models.EmailField(unique=True)
+
+
 class Task(models.Model):
     project = models.ForeignKey("Project",on_delete=models.CASCADE,default=1)
+    assigned_to = models.ManyToManyField(Employee,related_name='tasks')
     title = models.CharField(max_length=250)
     description = models.TextField()
     due_date = models.DateField()
@@ -19,7 +25,7 @@ class TaskDetail(models.Model):
         (MEDIUM,'Medium'),
         (LOW,'Low')
     )
-    task = models.OneToOneField(Task,on_delete=models.CASCADE)
+    task = models.OneToOneField(Task,on_delete=models.CASCADE,related_name='details' )
     assigned_to = models.CharField(max_length=100)
     priority = models.CharField(
         max_length=1,choices=PRIORITY_OPTIONS,default=LOW) 
