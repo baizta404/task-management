397e325a57ddfc5f3908ef3642031b8272d8ef9d
baizta404 <chowdhuryhridoy261@gmail.com>
2026-08-05 20:33:28 +0600

    6.1: GET & POST,6.2:Django Forms ,6.3:Save data in database from frontend

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tasks.forms import TaskForm
+from tasks.models import Employee,Task
 
 # Create your views here.
 def manager_dashboard(request):
@@ -13,3 +15,27 @@ def test(requset):
         'names':['Mahmud','Rahim','Rana','Mohsin','John','Iktiup','Zakir','Rohan']
     }
     return render(requset,'./test.html',context)
+
+def create_task(request):
+    employees = Employee.objects.all()
+    form = TaskForm(employees= employees)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST,employees=employees)
+        if form.is_valid():
+           data = form.cleaned_data
+           title = data.get('title')
+           description = data.get('description')
+           due_date = data.get('due_date')
+           assigned_to = data.get('assigned_to')
+           task = Task.objects.create(title=title,description=description,due_date=due_date)
+           #Assign employe to tasks
+           for emp_id in assigned_to:
+               employee = Employee.objects.get(id=emp_id)
+               task.assigned_to.add(employee)
+
+           return HttpResponse('Task Added Sucessfuly')
+    print(Task.objects.all())
+    context = {'form':form}
+    return render(request,'./taskform.html',context)
+#6.4 -> start korte hobe
