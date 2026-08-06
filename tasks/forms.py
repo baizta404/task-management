2e27b12f237717f48a654fd15eaadf8622dd23f5
baizta404 <chowdhuryhridoy261@gmail.com>
2026-08-07 00:52:05 +0600

    Module-6 Done: ModelForms,Widgets,Basic Mixin,Basic MRO

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from tasks.models import Task
 
 class TaskForm(forms.Form):
     title = forms.CharField(max_length=250,label='Task Title')
@@ -11,3 +12,65 @@ class TaskForm(forms.Form):
         super().__init__(*args, **kwargs )
         self.fields['assigned_to'].choices = [
             (emp.id,emp.name) for emp in employees]
+
+class StyledForMixin:
+    '''Mixin to apply style to form fields'''
+    default_clasees = "border-2 border-red-200 w-full p-2 bg-gray-300 rounded-lg shadow-sm"
+    def apply_styled_widgets(self):
+        for fied_name ,field in self.fields.items():
+            if isinstance(field.widget,forms.TextInput):
+                field.widget.attrs.update({
+                    'class':self.default_clasees,
+                    'placeholder':f'Enter {field.label.lower()}'
+                })
+            elif isinstance(field.widget,forms.Textarea):
+                field.widget.attrs.update({
+                    'class':self.default_clasees,
+                    'placeholder':f'Enter {field.label.lower()}'
+                })
+            elif isinstance(field.widget,forms.SelectDateWidget):
+                field.widget.attrs.update({
+                    'class':'border border-red-200 px-3 py-2 bg-gray-300 rounded-md shadow-sm'
+                })
+            elif isinstance(field.widget,forms.CheckboxSelectMultiple):
+                field.widget.attrs.update({
+                    'class': 'px-3 py-2 rounded-lg'
+                })
+            else:{
+                field.widget.attrs.update({
+                    'class':self.default_clasees
+                })
+            }
+            
+
+#Django Model Forms
+class TaskModelForm(StyledForMixin,forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title','description','due_date','assigned_to']
+        widgets={
+            'due_date':forms.SelectDateWidget,
+            'assigned_to':forms.CheckboxSelectMultiple
+        }
+        # exclude = ['project','is_completed','created_at','updated_at'] #egula baad diye baki gula dekhabe taile
+        ''' Manual Widget
+        # widgets = {
+        #     'title':forms.TextInput(attrs={
+        #         "class":"border-2 border-red-200 w-full p-2 bg-gray-300 rounded-lg shadow-sm","placeholder":"Enter Your Projects Title"
+        #     }),
+        #     'description':forms.Textarea(attrs={
+        #         "class":"border-2 border-red-200 p-2 w-full bg-gray-300 rounded-lg shadow-sm","placeholder":"Enter Your Descriptions",
+        #         "rows":5
+        #     }),
+        #     'due_date':forms.SelectDateWidget(attrs={
+        #         "class":"border-2 border-red-200 bg-gray-200 p-2 rounded-md shadow-sm"
+        #     }),
+        #     'assigned_to':forms.CheckboxSelectMultiple(attrs={
+        #         "class":"px-3 py-2 rounded-lg shadow-sm"
+        #     })
+        # } 
+         '''
+    '''Widget with Mixin'''
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.apply_styled_widgets()
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from tasks.forms import TaskForm
+from tasks.forms import TaskForm,TaskModelForm
 from tasks.models import Employee,Task
 
 # Create your views here.
@@ -17,25 +17,28 @@ def test(requset):
     return render(requset,'./test.html',context)
 
 def create_task(request):
-    employees = Employee.objects.all()
-    form = TaskForm(employees= employees)
+    # employees = Employee.objects.all() #model form a alada employees lage na
+    form = TaskModelForm()
 
     if request.method == 'POST':
-        form = TaskForm(request.POST,employees=employees)
+        form = TaskModelForm(request.POST)
         if form.is_valid():
-           data = form.cleaned_data
-           title = data.get('title')
-           description = data.get('description')
-           due_date = data.get('due_date')
-           assigned_to = data.get('assigned_to')
-           task = Task.objects.create(title=title,description=description,due_date=due_date)
-           #Assign employe to tasks
-           for emp_id in assigned_to:
-               employee = Employee.objects.get(id=emp_id)
-               task.assigned_to.add(employee)
+            '''For ModelForm'''
+            form.save()
+            return render(request,'./taskform.html',{'form':form,'message':'task added succesfully'})
+            
+            '''Eidi hocche Django Forms er Jonno ; ModelForm a Lage na'''
+        #    data = form.cleaned_data
+        #    title = data.get('title')
+        #    description = data.get('description')
+        #    due_date = data.get('due_date')
+        #    assigned_to = data.get('assigned_to')
+        #    task = Task.objects.create(title=title,description=description,due_date=due_date)
+        #    #Assign employe to tasks
+        #    for emp_id in assigned_to:
+        #        employee = Employee.objects.get(id=emp_id)
+        #        task.assigned_to.add(employee)
 
-           return HttpResponse('Task Added Sucessfuly')
-    print(Task.objects.all())
+            # return HttpResponse('Task Added Sucessfuly')
     context = {'form':form}
     return render(request,'./taskform.html',context)
-#6.4 -> start korte hobe
