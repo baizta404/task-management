d7ebbe3065689b47601b57cfa3ad0ab38e2790ae
baizta404 <chowdhuryhridoy261@gmail.com>
2026-08-21 17:46:20 +0600

    Reset Database & some other upgrades

--- tasks/models.py
+++ tasks/models.py
@@ -9,14 +9,23 @@ class Employee(models.Model):
 
 
 class Task(models.Model):
+    STATUS_CHOICES =[
+        ('PENDING','Pending'),
+        ('IN_PROGRESS','In Progress'),
+        ('COMPLETED','Completed')
+    ]
     project = models.ForeignKey("Project",on_delete=models.CASCADE,default=1)
     assigned_to = models.ManyToManyField(Employee,related_name='tasks')
     title = models.CharField(max_length=250)
     description = models.TextField()
     due_date = models.DateField()
+    status = models.CharField(max_length=15,choices=STATUS_CHOICES,default='PENDING')
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
 
 
 class TaskDetail(models.Model):
@@ -31,8 +40,18 @@ class TaskDetail(models.Model):
     task = models.OneToOneField(Task,on_delete=models.CASCADE,related_name='details' )
     assigned_to = models.CharField(max_length=100)
     priority = models.CharField(
-        max_length=1,choices=PRIORITY_OPTIONS,default=LOW) 
+        max_length=1,choices=PRIORITY_OPTIONS,default=LOW)
+    notes = models.TextField(blank=True,null=True)
+
+    def __str__(self):
+        return f'Details for Task {self.task.title}'
 
 class Project(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField(blank=True,null=True)
     start_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+#7.3 retriving => start
